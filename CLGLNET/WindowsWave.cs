fc596f2308bad693d452e6e688d37a01511eb9b9
ptpatryk <55487007+ptpatryk@users.noreply.github.com>
2024-12-22 20:52:24 +0100

    jakieś takie programowanie

--- CLGLNET/WindowsWave.cs
+++ CLGLNET/WindowsWave.cs
@@ -1,13 +1,10 @@
-﻿//using OpenTK.Compute.OpenCL;
-//using OpenTK.Mathematics;
-//using OpenTK.Windowing.Common;
-//using OpenTK.Windowing.Desktop;
+﻿//using OpenTK.Mathematics;
 //using System;
 //using System.Collections.Generic;
 //using System.Linq;
 //using System.Text;
 //using System.Threading.Tasks;
-//using static OpenTK.Graphics.OpenGL.GL;
+
 //using OpenTK.Graphics.OpenGL4;
 //using Cloo;
 using OpenTK;
@@ -60,6 +57,10 @@ namespace CLGLNET
         int N_X = 100;
         int N_Y = 100;
 
+        private int _vertexBufferObject;
+        private int _vertexArrayObject;
+        private int _shaderProgram;
+
         public WindowsWave(int width, int height, string title)
             : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = new OpenTK.Mathematics.Vector2i(width, height), Title = title })
         {
@@ -87,21 +88,109 @@ namespace CLGLNET
         {
             base.OnLoad();
 
+            
             // Inicjalizacja OpenGL
-            InitOpenGL();
+            //InitOpenGL();
 
             // Inicjalizacja OpenCL
-            InitOpenCL();
+            //InitOpenCL();
 
             // Przykładowe dane
             Punkt[] aa = new Punkt[N_X * N_Y];
+
+            for (int i = 0; i < aa.Length; i++)
+            {
+                aa[i].m = 1.0f;
+            }
+
+
             float dt = 0.01f;
             float w = -2.0f;
             //int N_X = 100;
             //int N_Y = 100;
 
             // Uruchomienie kernela
-            RunKernel(aa, dt, w, N_X, N_Y);
+            //RunKernel(aa, dt, w, N_X, N_Y);
+
+
+            //    float[] vertices = {
+            //    // Wierzchołki i normalne
+            //    // x, y, z, nx, ny, nz
+            //    0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,
+            //   -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,
+            //    0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f
+            //};
+
+            float[] vertices = {
+    // Trójkąt 1
+    -0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,
+   -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,
+    0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,
+    // Trójkąt 2
+    -1.0f, 1.0f, 0.0f,  0.0f, 0.0f, 1.0f,
+    1.0f,  -1.0f, 0.0f,  0.0f, 0.0f, 1.0f,
+    1.0f, 1.0f, 0.0f,  0.0f, 0.0f, 1.0f,
+};
+
+            _vertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            _vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArrayObject);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 3 * sizeof(float));
+            GL.EnableVertexAttribArray(1);
+
+            string vertexShaderSource = @"
+        #version 330 core
+        layout(location = 0) in vec3 aPosition;
+        layout(location = 1) in vec3 aNormal;
+        void main()
+        {
+            gl_Position = vec4(aPosition, 1.0);
+        }";
+
+            string fragmentShaderSource = @"
+        #version 330 core
+        out vec4 FragColor;
+        void main()
+        {
+            FragColor = vec4(1.0, 0.5, 0.2, 1.0);
+        }";
+
+            int vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShader, vertexShaderSource);
+            GL.CompileShader(vertexShader);
+
+            int fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShader, fragmentShaderSource);
+            GL.CompileShader(fragmentShader);
+
+            _shaderProgram = GL.CreateProgram();
+            GL.AttachShader(_shaderProgram, vertexShader);
+            GL.AttachShader(_shaderProgram, fragmentShader);
+            GL.LinkProgram(_shaderProgram);
+
+            GL.DeleteShader(vertexShader);
+            GL.DeleteShader(fragmentShader);
+        }
+
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.UseProgram(_shaderProgram);
+            GL.BindVertexArray(_vertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 6);//rysuje 6 wierzchołków (2 trójkąty)
+
+            SwapBuffers();
         }
 
         unsafe void InitOpenGL()
@@ -134,6 +223,16 @@ namespace CLGLNET
             */
             var platform = ComputePlatform.Platforms[0];
             var devices = platform.Devices;
+
+            //// Get the current OpenGL context handle
+            //IntPtr glContextHandle = OpenTK.Graphics.OpenGL.GL.GetCurrentContext();
+
+            //// Create the OpenCL context properties with OpenGL context
+            //var properties = new ComputeContextPropertyList(platform);
+            //properties.Add(new ComputeContextProperty(ComputeContextPropertyName.CL_GL_CONTEXT_KHR, glContextHandle));
+            //properties.Add(new ComputeContextProperty(ComputeContextPropertyName.CL_WGL_HDC_KHR, OpenTK.Platform.Windows.API.GetCurrentDC()));
+
+
             clContext = new ComputeContext(devices, new ComputeContextPropertyList(platform), null, IntPtr.Zero);
             queue = new ComputeCommandQueue(clContext, devices[0], ComputeCommandQueueFlags.None);
 
@@ -163,10 +262,16 @@ namespace CLGLNET
             queue.AcquireGLObjects(new[] { clNbo }, null);
 
             var globalWorkSize = new long[] { N_X, N_Y };
+
             queue.Execute(kernel, null, globalWorkSize, null, null);
+
+            //Normal[] normals = new Normal[N_X * N_Y]; // Initialize the array
+            //queue.ReadFromBuffer(clNbo, ref normals, true, null);
 
             queue.ReleaseGLObjects(new[] { clNbo }, null);
             queue.Finish();
+
+            //Render();
         }
 
         unsafe void Render()
