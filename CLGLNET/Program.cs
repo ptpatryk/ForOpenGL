d8ea5a29e284dbe5fb880ef618b8b4f6281421b5
ptpatryk <55487007+ptpatryk@users.noreply.github.com>
2024-12-22 17:32:36 +0100

    na dzisiaj mam dość

--- CLGLNET/Program.cs
+++ CLGLNET/Program.cs
@@ -1,54 +1,71 @@
-﻿//using OpenTK;
-//using OpenTK.Graphics.OpenGL4;
-//using OpenTK.Compute.OpenCL;
-//using System;
-//using System.IO;
-//using static OpenTK.Graphics.OpenGL.GL;
-////using static OpenTK.Compute.OpenCL.CL;
-//using System.Runtime.CompilerServices;
-//using System.Runtime.InteropServices;
-//using System;
-//using System.IO;
-
-using OpenTK;
+﻿using OpenTK;
 using OpenTK.Graphics.OpenGL4;
-using OpenTK.Compute.OpenCL;
+using Cloo;
 using System;
 using System.IO;
-
+using System.Runtime.InteropServices;
+using static OpenTK.Graphics.OpenGL.GL;
+using OpenTK.Windowing.Desktop;
+using CLGLNET;
 
 class Program
 {
-    static int vbo, nbo;
-    static CLContext clContext;
-    static CLCommandQueue queue;
-    static CLKernel kernel;
-    static CLBuffer aaBuf, bbBuf, clVbo, clNbo;
+    //static int vbo, nbo;
+    //static ComputeContext clContext;
+    //static ComputeCommandQueue queue;
+    //static ComputeKernel kernel;
+    //static ComputeBuffer<Punkt> aaBuf, bbBuf;
+    //static ComputeBuffer<Normal> clNbo;
 
-    static int N_X = 100;
-    static int N_Y = 100;
+
 
 
     static void Main(string[] args)
     {
+        using (var game = new WindowsWave(800, 600, "OpenGL with OpenCL"))
+        {
+            game.Run();
+        }
+
+
         // Inicjalizacja OpenGL
-        InitOpenGL();
+        //InitOpenGL();
 
-        // Inicjalizacja OpenCL
-        InitOpenCL();
+        /*
+        var nativeWindowSettings = new NativeWindowSettings()
+        {
+            Size = new OpenTK.Mathematics.Vector2i(800, 600),
+            Title = "OpenGL Example"
+        };
+        */
+        /*
+        using (var game = new GameWindow(800, 600, GraphicsMode.Default, "OpenGL Example"))
+        {
+            game.Load += (sender, e) =>
+            {
+                // Initialize OpenGL here
+                InitOpenGL();
+            };
 
-        // Przykładowe dane
-        Punkt[] aa = new Punkt[N_X * N_Y];
-        float dt = 0.01f;
-        float w = -2.0f;
+            game.Run(60.0);
+        }
+        */
+        //// Inicjalizacja OpenCL
+        //InitOpenCL();
 
-        // Uruchomienie kernela
-        RunKernel(aa, dt, w, N_X, N_Y);
+        //// Przykładowe dane
+        //Punkt[] aa = new Punkt[N_X * N_Y];
+        //float dt = 0.01f;
+        //float w = -2.0f;
 
-        // Renderowanie
-        Render();
+        //// Uruchomienie kernela
+        //RunKernel(aa, dt, w, N_X, N_Y);
+
+        //// Renderowanie
+        //Render();
     }
 
+    /*
     static unsafe void InitOpenGL()
     {
         GL.GenBuffers(1, out vbo);
@@ -62,67 +79,47 @@ class Program
 
     static void InitOpenCL()
     {
-        unsafe
-        {
+        var platform = ComputePlatform.Platforms[0];
+        var devices = platform.Devices;
+        clContext = new ComputeContext(devices, new ComputeContextPropertyList(platform), null, IntPtr.Zero);
+        queue = new ComputeCommandQueue(clContext, devices[0], ComputeCommandQueueFlags.None);
 
-            var platformIds = CL.GetPlatformIds(out var platforms);
-            if (platformIds != CLResultCode.Success)
-            {
-                throw new Exception("Failed to get OpenCL platforms.");
-            }
+        string kernelSource = File.ReadAllText("kernel.cl");
+        var program = new ComputeProgram(clContext, kernelSource);
+        program.Build(devices, null, null, IntPtr.Zero);
+        kernel = program.CreateKernel("obliczWspolrzedneINormalne");
 
-            var deviceIds = CL.GetDeviceIds(platforms[0], DeviceType.Gpu, out var devices);
-            if (deviceIds != CLResultCode.Success)
-            {
-                throw new Exception("Failed to get OpenCL devices.");
-            }
-
-            clContext = CL.CreateContext(IntPtr.Zero, devices, IntPtr.Zero, IntPtr.Zero, out var resultCode);
-            if (resultCode != CLResultCode.Success)
-            {
-                throw new Exception("Failed to create OpenCL context.");
-            }
-            queue = CL.CreateCommandQueue(clContext, devices[0],CommandQueueProperties.None);
-
-            string kernelSource = File.ReadAllText("kernel.cl");
-            var program = CL.CreateProgramWithSource(clContext, kernelSource);
-            CL.BuildProgram(program, devices, string.Empty);
-            kernel = CL.CreateKernel(program, "obliczWspolrzedneINormalne");
-
-            aaBuf = CL.CreateBuffer(clContext, MemFlags.ReadOnly, sizeof(Punkt) * N_X * N_Y);
-            bbBuf = CL.CreateBuffer(clContext, MemFlags.ReadWrite, sizeof(Punkt) * N_X * N_Y);
-            clVbo = CL.CreateFromGLBuffer(clContext, MemFlags.WriteOnly, vbo);
-            clNbo = CL.CreateFromGLBuffer(clContext, MemFlags.WriteOnly, nbo);
-
-        }
+        aaBuf = new ComputeBuffer<Punkt>(clContext, ComputeMemoryFlags.ReadOnly | ComputeMemoryFlags.CopyHostPointer, new Punkt[N_X * N_Y]);
+        bbBuf = new ComputeBuffer<Punkt>(clContext, ComputeMemoryFlags.ReadWrite, N_X * N_Y);
+        clNbo = new ComputeBuffer<Normal>(clContext, ComputeMemoryFlags.WriteOnly, N_X * N_Y);
     }
 
     static unsafe void RunKernel(Punkt[] aa, float dt, float w, int N_X, int N_Y)
     {
-        CL.EnqueueWriteBuffer(queue, aaBuf, true, 0, sizeof(Punkt) * N_X * N_Y, aa);
+        queue.WriteToBuffer(aa, aaBuf, true, null);
 
-        CL.SetKernelArg(kernel, 0, aaBuf);
-        CL.SetKernelArg(kernel, 1, bbBuf);
-        CL.SetKernelArg(kernel, 2, clNbo);
-        CL.SetKernelArg(kernel, 3, dt);
-        CL.SetKernelArg(kernel, 4, w);
-        CL.SetKernelArg(kernel, 5, N_X);
-        CL.SetKernelArg(kernel, 6, N_Y);
+        kernel.SetMemoryArgument(0, aaBuf);
+        kernel.SetMemoryArgument(1, bbBuf);
+        kernel.SetMemoryArgument(2, clNbo);
+        kernel.SetValueArgument(3, dt);
+        kernel.SetValueArgument(4, w);
+        kernel.SetValueArgument(5, N_X);
+        kernel.SetValueArgument(6, N_Y);
 
         GL.Finish();
-        CL.EnqueueAcquireGLObjects(queue, new[] { clVbo, clNbo });
+        queue.AcquireGLObjects(new[] { clNbo }, null);
 
-        var globalWorkSize = new IntPtr[] { (IntPtr)N_X, (IntPtr)N_Y };
-        CL.EnqueueNDRangeKernel(queue, kernel, 2, null, globalWorkSize, null);
+        var globalWorkSize = new long[] { N_X, N_Y };
+        queue.Execute(kernel, null, globalWorkSize, null, null);
 
-        CL.EnqueueReleaseGLObjects(queue, new[] { clVbo, clNbo });
-        CL.Finish(queue);
+        queue.ReleaseGLObjects(new[] { clNbo }, null);
+        queue.Finish();
     }
 
-    static void Render()
+    static unsafe void Render()
     {
-        unsafe
-        {
+        
+
             GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
             GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, sizeof(Punkt), IntPtr.Zero);
             GL.EnableVertexAttribArray(0);
@@ -130,19 +127,8 @@ class Program
             GL.BindBuffer(BufferTarget.ArrayBuffer, nbo);
             GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, sizeof(Normal), IntPtr.Zero);
             GL.EnableVertexAttribArray(1);
-        }
         // ... (rysowanie obiektów)
     }
+    */
 }
 
-[StructLayout(LayoutKind.Sequential)]
-struct Punkt
-{
-    public float m, v, x;
-}
-
-[StructLayout(LayoutKind.Sequential)]
-struct Normal
-{
-    public float nx, ny, nz;
-}
---
+++ CLGLNET/WindowsWave.cs
@@ -0,0 +1,259 @@
+﻿//using OpenTK.Compute.OpenCL;
+//using OpenTK.Mathematics;
+//using OpenTK.Windowing.Common;
+//using OpenTK.Windowing.Desktop;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using static OpenTK.Graphics.OpenGL.GL;
+//using OpenTK.Graphics.OpenGL4;
+//using Cloo;
+using OpenTK;
+using OpenTK.Graphics.OpenGL4;
+using Cloo;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using static OpenTK.Graphics.OpenGL.GL;
+using OpenTK.Windowing.Desktop;
+//using OpenTK;
+//using OpenTK.Graphics.OpenGL4;
+using OpenTK.Compute.OpenCL;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using OpenTK.Windowing.Common;
+
+
+namespace CLGLNET
+{
+    [StructLayout(LayoutKind.Sequential)]
+    struct Punkt
+    {
+        public float m, v, x;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct Normal
+    {
+        public float nx, ny, nz;
+    }
+
+
+    public class WindowsWave : GameWindow
+    {
+        //int vbo, nbo;
+        //CLContext clContext;
+        //CLCommandQueue queue;
+        //CLKernel kernel;
+        //CLBuffer aaBuf, bbBuf, clNbo;
+
+        int vbo, nbo;
+        ComputeContext clContext;
+        ComputeCommandQueue queue;
+        ComputeKernel kernel;
+        ComputeBuffer<Punkt> aaBuf, bbBuf;
+        ComputeBuffer<Normal> clNbo;
+
+        int N_X = 100;
+        int N_Y = 100;
+
+        public WindowsWave(int width, int height, string title)
+            : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = new OpenTK.Mathematics.Vector2i(width, height), Title = title })
+        {
+            //this.Load += () =>
+            //{
+            //    // Initialize OpenGL here
+            //    InitOpenGL();
+            //    // Inicjalizacja OpenCL
+            //    InitOpenCL();
+            //    // Przykładowe dane
+            //    Punkt[] aa = new Punkt[N_X * N_Y];
+            //    float dt = 0.01f;
+            //    float w = -2.0f;
+            //    // Uruchomienie kernela
+            //    RunKernel(aa, dt, w, N_X, N_Y);
+            //};
+
+        }
+
+
+
+
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            // Inicjalizacja OpenGL
+            InitOpenGL();
+
+            // Inicjalizacja OpenCL
+            InitOpenCL();
+
+            // Przykładowe dane
+            Punkt[] aa = new Punkt[N_X * N_Y];
+            float dt = 0.01f;
+            float w = -2.0f;
+            //int N_X = 100;
+            //int N_Y = 100;
+
+            // Uruchomienie kernela
+            RunKernel(aa, dt, w, N_X, N_Y);
+        }
+
+        unsafe void InitOpenGL()
+        {
+            GL.GenBuffers(1, out vbo);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, sizeof(Punkt) * N_X * N_Y, IntPtr.Zero, BufferUsageHint.DynamicDraw);
+
+            GL.GenBuffers(1, out nbo);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, nbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, sizeof(Normal) * N_X * N_Y, IntPtr.Zero, BufferUsageHint.DynamicDraw);
+        }
+
+        unsafe void InitOpenCL()
+        {
+            /*
+            var platform = ComputePlatform.Platforms[0];
+            var devices = platform.Devices;
+            clContext = new ComputeContext(devices, new ComputeContextPropertyList(platform), null, IntPtr.Zero);
+            queue = new ComputeCommandQueue(clContext, devices[0], ComputeCommandQueueFlags.None);
+
+            string kernelSource = File.ReadAllText("kernel.cl");
+            var program = new ComputeProgram(clContext, kernelSource);
+            program.Build(devices, null, null, IntPtr.Zero);
+            kernel = program.CreateKernel("obliczWspolrzedneINormalne");
+
+            aaBuf = new ComputeBuffer<Punkt>(clContext, ComputeMemoryFlags.ReadOnly | ComputeMemoryFlags.CopyHostPointer, new Punkt[N_X * N_Y]);
+            bbBuf = new ComputeBuffer<Punkt>(clContext, ComputeMemoryFlags.ReadWrite, N_X * N_Y);
+            clNbo = new ComputeBuffer<Normal>(clContext, ComputeMemoryFlags.WriteOnly, N_X * N_Y);
+            */
+            var platform = ComputePlatform.Platforms[0];
+            var devices = platform.Devices;
+            clContext = new ComputeContext(devices, new ComputeContextPropertyList(platform), null, IntPtr.Zero);
+            queue = new ComputeCommandQueue(clContext, devices[0], ComputeCommandQueueFlags.None);
+
+            string kernelSource = File.ReadAllText("kernel.cl");
+            var program = new ComputeProgram(clContext, kernelSource);
+            program.Build(devices, null, null, IntPtr.Zero);
+            kernel = program.CreateKernel("obliczWspolrzedneINormalne");
+
+            aaBuf = new ComputeBuffer<Punkt>(clContext, ComputeMemoryFlags.ReadOnly | ComputeMemoryFlags.CopyHostPointer, new Punkt[N_X * N_Y]);
+            bbBuf = new ComputeBuffer<Punkt>(clContext, ComputeMemoryFlags.ReadWrite, N_X * N_Y);
+            clNbo = new ComputeBuffer<Normal>(clContext, ComputeMemoryFlags.WriteOnly, N_X * N_Y);
+        }
+
+        unsafe void RunKernel(Punkt[] aa, float dt, float w, int N_X, int N_Y)
+        {
+            queue.WriteToBuffer(aa, aaBuf, true, null);
+
+            kernel.SetMemoryArgument(0, aaBuf);
+            kernel.SetMemoryArgument(1, bbBuf);
+            kernel.SetMemoryArgument(2, clNbo);
+            kernel.SetValueArgument(3, dt);
+            kernel.SetValueArgument(4, w);
+            kernel.SetValueArgument(5, N_X);
+            kernel.SetValueArgument(6, N_Y);
+
+            GL.Finish();
+            queue.AcquireGLObjects(new[] { clNbo }, null);
+
+            var globalWorkSize = new long[] { N_X, N_Y };
+            queue.Execute(kernel, null, globalWorkSize, null, null);
+
+            queue.ReleaseGLObjects(new[] { clNbo }, null);
+            queue.Finish();
+        }
+
+        unsafe void Render()
+        {
+
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, sizeof(Punkt), IntPtr.Zero);
+            GL.EnableVertexAttribArray(0);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, nbo);
+            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, sizeof(Normal), IntPtr.Zero);
+            GL.EnableVertexAttribArray(1);
+            // ... (rysowanie obiektów)
+        }
+
+        /*
+
+        static unsafe void InitOpenGL()
+        {
+            GL.GenBuffers(1, out vbo);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, sizeof(Punkt) * N_X * N_Y, IntPtr.Zero, BufferUsageHint.DynamicDraw);
+
+            GL.GenBuffers(1, out nbo);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, nbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, sizeof(Normal) * N_X * N_Y, IntPtr.Zero, BufferUsageHint.DynamicDraw);
+        }
+
+        static void InitOpenCL()
+        {
+            var platforms = CL.GetPlatformIDs();
+            var devices = CL.GetDeviceIDs(platforms[0], DeviceType.Gpu);
+            clContext = CL.CreateContext(null, devices, null, IntPtr.Zero);
+            queue = CL.CreateCommandQueue(clContext, devices[0], CommandQueueProperties.None);
+
+            string kernelSource = File.ReadAllText("kernel.cl");
+            var program = CL.CreateProgramWithSource(clContext, kernelSource);
+            CL.BuildProgram(program, devices, string.Empty);
+            kernel = CL.CreateKernel(program, "obliczWspolrzedneINormalne");
+
+            aaBuf = CL.CreateBuffer(clContext, MemFlags.ReadOnly, sizeof(Punkt) * N_X * N_Y);
+            bbBuf = CL.CreateBuffer(clContext, MemFlags.ReadWrite, sizeof(Punkt) * N_X * N_Y);
+            clNbo = CL.CreateBuffer(clContext, MemFlags.WriteOnly, sizeof(Normal) * N_X * N_Y);
+        }
+
+        static unsafe void RunKernel(Punkt[] aa, float dt, float w, int N_X, int N_Y)
+        {
+            CL.EnqueueWriteBuffer(queue, aaBuf, true, 0, sizeof(Punkt) * N_X * N_Y, aa);
+
+            CL.SetKernelArg(kernel, 0, aaBuf);
+            CL.SetKernelArg(kernel, 1, bbBuf);
+            CL.SetKernelArg(kernel, 2, clNbo);
+            CL.SetKernelArg(kernel, 3, dt);
+            CL.SetKernelArg(kernel, 4, w);
+            CL.SetKernelArg(kernel, 5, N_X);
+            CL.SetKernelArg(kernel, 6, N_Y);
+
+            GL.Finish();
+            CL.EnqueueAcquireGLObjects(queue, new[] { clNbo });
+
+            var globalWorkSize = new IntPtr[] { (IntPtr)N_X, (IntPtr)N_Y };
+            CL.EnqueueNDRangeKernel(queue, kernel, 2, null, globalWorkSize, null);
+
+            CL.EnqueueReleaseGLObjects(queue, new[] { clNbo });
+            CL.Finish(queue);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, sizeof(Punkt), IntPtr.Zero);
+            GL.EnableVertexAttribArray(0);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, nbo);
+            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, sizeof(Normal), IntPtr.Zero);
+            GL.EnableVertexAttribArray(1);
+
+            // ... (rysowanie obiektów)
+
+            SwapBuffers();
+        }
+        */
+    }
+
+}
