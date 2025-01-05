c8d78a509e7528d2f40ed80f38a955856510f98c
ptpatryk <55487007+ptpatryk@users.noreply.github.com>
2025-01-05 21:15:13 +0100

    zaczątki w directx

---
+++ ConsoleApplication1/WindowsWaveDirect.cpp
@@ -0,0 +1,184 @@
+#include "WindowsWaveDirect.h"
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <math.h>
+#include <chrono>
+#include <thread>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+#include "ForWindow.cpp"
+
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+
+LRESULT CALLBACK WindowProcFor(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+WindowsWaveDirect::WindowsWaveDirect(int width, int height, const std::string& title) {
+    // Register the window class
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProcFor;
+    wc.hInstance = GetModuleHandle(NULL);
+    wc.lpszClassName = L"WindowsWaveClass";
+    RegisterClass(&wc);
+
+    this->width = width;
+	this->height = height;
+
+    // Create the window
+    hwnd = CreateWindowEx(
+        0,
+        L"WindowsWaveClass",
+        std::wstring(title.begin(), title.end()).c_str(),
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, width, height,
+        NULL,
+        NULL,
+        GetModuleHandle(NULL),
+        NULL
+    );
+
+    if (!hwnd) {
+        std::cerr << "Failed to create window" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    ShowWindow(hwnd, SW_SHOW);
+
+    // Initialize DirectX
+    InitDirectX();
+    InitDirectCompute();
+}
+
+WindowsWaveDirect::~WindowsWaveDirect() {
+    // Clean up DirectX resources
+    if (deviceContext) deviceContext->ClearState();
+    if (swapChain) swapChain->Release();
+    if (renderTargetView) renderTargetView->Release();
+    if (deviceContext) deviceContext->Release();
+    if (device) device->Release();
+}
+
+void WindowsWaveDirect::InitDirectX() {
+    // Create device and swap chain
+    DXGI_SWAP_CHAIN_DESC scd = {};
+    scd.BufferCount = 1;
+    scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    scd.OutputWindow = hwnd;
+    scd.SampleDesc.Count = 1;
+    scd.Windowed = TRUE;
+
+    D3D11CreateDeviceAndSwapChain(
+        NULL,
+        D3D_DRIVER_TYPE_HARDWARE,
+        NULL,
+        0,
+        NULL,
+        0,
+        D3D11_SDK_VERSION,
+        &scd,
+        &swapChain,
+        &device,
+        NULL,
+        &deviceContext
+    );
+
+    // Create render target view
+    ID3D11Texture2D* backBuffer;
+    swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&backBuffer);
+    device->CreateRenderTargetView(backBuffer, NULL, &renderTargetView);
+    backBuffer->Release();
+
+    deviceContext->OMSetRenderTargets(1, &renderTargetView, NULL);
+
+    // Set viewport
+    D3D11_VIEWPORT viewport = {};
+    viewport.Width = (FLOAT)width;
+    viewport.Height = (FLOAT)height;
+    viewport.MinDepth = 0.0f;
+    viewport.MaxDepth = 1.0f;
+    viewport.TopLeftX = 0;
+    viewport.TopLeftY = 0;
+    deviceContext->RSSetViewports(1, &viewport);
+}
+
+void WindowsWaveDirect::InitDirectCompute() {
+    // Create compute shader
+    ID3DBlob* csBlob = nullptr;
+    D3DCompileFromFile(L"compute_shader.hlsl", NULL, NULL, "CSMain", "cs_5_0", 0, 0, &csBlob, NULL);
+    device->CreateComputeShader(csBlob->GetBufferPointer(), csBlob->GetBufferSize(), NULL, &computeShader);
+    csBlob->Release();
+
+    // Create buffers
+    D3D11_BUFFER_DESC bufferDesc = {};
+    bufferDesc.Usage = D3D11_USAGE_DEFAULT;
+    bufferDesc.ByteWidth = sizeof(Punkt) * N_X * N_Y;
+    bufferDesc.BindFlags = D3D11_BIND_UNORDERED_ACCESS | D3D11_BIND_SHADER_RESOURCE;
+    bufferDesc.CPUAccessFlags = 0;
+
+    D3D11_SUBRESOURCE_DATA initData = {};
+    initData.pSysMem = aa;
+    device->CreateBuffer(& bufferDesc, & initData, & aaBuffer);
+    device->CreateBuffer(&bufferDesc, NULL, &bbBuffer);
+
+    D3D11_UNORDERED_ACCESS_VIEW_DESC uavDesc = {};
+    uavDesc.Format = DXGI_FORMAT_UNKNOWN;
+    uavDesc.ViewDimension = D3D11_UAV_DIMENSION_BUFFER;
+    uavDesc.Buffer.FirstElement = 0;
+    uavDesc.Buffer.NumElements = N_X * N_Y;
+
+    device->CreateUnorderedAccessView(aaBuffer, &uavDesc, &aaUAV);
+    device->CreateUnorderedAccessView(bbBuffer, &uavDesc, &bbUAV);
+}
+
+void WindowsWaveDirect::OnRenderFrame() {
+    float clearColor[4] = { 0.0f, 0.2f, 0.4f, 1.0f };
+    deviceContext->ClearRenderTargetView(renderTargetView, clearColor);
+
+    // Render your scene here
+
+    swapChain->Present(1, 0);
+}
+
+void WindowsWaveDirect::OnUpdateFrame() {
+    RunKernel();
+    czas += dt;
+}
+
+void WindowsWaveDirect::RunKernel() {
+    deviceContext->CSSetShader(computeShader, NULL, 0);
+    deviceContext->CSSetUnorderedAccessViews(0, 1, &aaUAV, NULL);
+    deviceContext->CSSetUnorderedAccessViews(1, 1, &bbUAV, NULL);
+
+    deviceContext->Dispatch(N_X, N_Y, 1);
+
+    // Swap buffers
+    std::swap(aaUAV, bbUAV);
+}
+
+void WindowsWaveDirect::Run() {
+    MSG msg = {};
+    while (msg.message != WM_QUIT) {
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+        else {
+            OnUpdateFrame();
+            OnRenderFrame();
+        }
+    }
+}
---
+++ ConsoleApplication1/WindowsWaveDirect.h
@@ -0,0 +1,49 @@
+#ifndef WINDOWSWAVEDIRECT_H
+#define WINDOWSWAVEDIRECT_H
+
+#include <string>
+#include <d3d11.h>
+#include <DirectXMath.h>
+
+class WindowsWaveDirect {
+public:
+    WindowsWaveDirect(int width, int height, const std::string& title);
+    ~WindowsWaveDirect();
+
+    void Run();
+
+private:
+    void InitDirectX();
+    void InitDirectCompute();
+    void OnRenderFrame();
+    void OnUpdateFrame();
+    void RunKernel();
+
+	int width;
+	int height;
+
+    HWND hwnd;
+    ID3D11Device* device = nullptr;
+    ID3D11DeviceContext* deviceContext = nullptr;
+    IDXGISwapChain* swapChain = nullptr;
+    ID3D11RenderTargetView* renderTargetView = nullptr;
+    ID3D11ComputeShader* computeShader = nullptr;
+    ID3D11Buffer* aaBuffer = nullptr;
+    ID3D11Buffer* bbBuffer = nullptr;
+    ID3D11UnorderedAccessView* aaUAV = nullptr;
+    ID3D11UnorderedAccessView* bbUAV = nullptr;
+
+    struct Punkt {
+        float m;
+        float v;
+        float x;
+    };
+
+    Punkt* aa = nullptr;
+    float czas = 0.0f;
+    float dt = 0.01f;
+    int N_X = 100;
+    int N_Y = 100;
+};
+
+#endif // WINDOWSWAVEDIRECT_H
