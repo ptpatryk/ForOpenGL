c60c013c2098501a82764fab2ebabdd59767cb73
ptpatryk <55487007+ptpatryk@users.noreply.github.com>
2025-01-10 17:37:47 +0100

    takie

--- ConsoleApplication1/VertexShader.hlsl
+++ ConsoleApplication1/VertexShader.hlsl
@@ -34,11 +34,13 @@ cbuffer ConstantBuffer : register(b0)
 struct VS_INPUT
 {
     float4 Pos : POSITION;
+    float3 Normal : NORMAL;
 };
 
 struct PS_INPUT
 {
     float4 Pos : SV_POSITION;
+    float3 Normal : NORMAL;
 };
 
 //PS_INPUT VS(VS_INPUT input)
