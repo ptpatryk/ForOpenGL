b67d527981432075acd382c6c7477c37cef2418c
ptpatryk <55487007+ptpatryk@users.noreply.github.com>
2025-01-18 16:35:44 +0100

    wydzielenie projektu w direct

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -9,38 +9,13 @@
 #include <thread>
 #include <chrono>
 
-//#include "WindowsWave.h"
-#include "WindowsWaveDirect.h"
+#include "WindowsWave.h"
+
 
 
 int main(int argc, char** argv) {
-	//WindowsWave wave(1600, 1200, "WindowsWave");
-	WindowsWaveDirect wave(1600, 1200, "WindowsWave");
+	WindowsWave wave(1600, 1200, "WindowsWave");
+	//WindowsWaveDirect wave(1600, 1200, "WindowsWave");
 	wave.Run();
 	return 0;
-	/*glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowSize(800, 600);
-	glutCreateWindow("3D Triangle with Shaders");
-
-	glewInit();
-	init();
-
-	
-
-	myfale = new Fale();
-	myfale->Inicjuj(1);
-	myfale->InicjujStrune(dwa_osrodki, 1);
-
-	aa=myfale->PogiezPlat();
-
-	glutDisplayFunc(display);
-	glutReshapeFunc(reshape);
-
-std::thread myThread(threadFunction);
-
-	glutMainLoop();
-	myThread.join();
-
-	return 0;*/
 }
